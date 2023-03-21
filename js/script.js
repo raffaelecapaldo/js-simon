f127ed0a163020b6a3de15d2836f10bc9903a611
raffaelecapaldo <raffaelecpl@gmail.com>
2023-03-21 14:08:14 +0100

    Added function to generate Numbers and show them

--- js/script.js
+++ js/script.js
@@ -2,4 +2,26 @@
 Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, i numeri che ha visto precedentemente.
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 */
+const playButton = document.getElementById("play")
 
+//Funzione per generare TOT numeri casuali da 1 a 100
+function generateNumbers(howManyNumbers) {
+  const numbers = [];
+  while (numbers.length < howManyNumbers) {
+      const number = getRandomInt(1, 100);
+      if (!numbers.includes(number)) {
+          numbers.push(number);
+      }
+  }
+  return numbers;
+}
+
+playButton.addEventListener("click", playGame)
+
+function playGame () {
+  NUMBERS = 5;
+  guessNumbers = generateNumbers(NUMBERS);
+  const boxNumbers = document.getElementById("numbers");
+  for(let i = 0; i < NUMBERS; i++)
+  boxNumbers.innerHTML += `<button type="button" class="btn btn-dark">${guessNumbers[i]}</button>`
+}
